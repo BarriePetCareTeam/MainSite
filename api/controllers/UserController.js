744d90ce36dfb389cdf3b22817f7a14e61ca77bf
Brian Jacildo <brian_jacildo@yahoo.com>
2015-10-14 11:34:51 -0400

    Add Create functionality to User Controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,3 +1,5 @@
+/*jslint node:true */
+
 /**
  * UserController
  *
@@ -5,7 +7,22 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+'use strict';
+
 module.exports = {
 
+    //PUT - Create new user
+    create: function (req, res, next) {
+        var params = req.params.all();
+
+        User.create(params, function (err, user) {
+            if (err) {
+                return next(err);
+            }
+            res.status(201);
+            res.json(user);
+        });
+
+    }
 };
 
